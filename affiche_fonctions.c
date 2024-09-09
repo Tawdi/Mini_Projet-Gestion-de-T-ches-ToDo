872a4cf59f1bd27eed2a2e0e3283728c6fe571de
ahmed <ahmedtawdi00@gmail.com>
2024-09-09 03:56:40 +0100

    affichage est modifié

--- affiche_fonctions.c
+++ affiche_fonctions.c
@@ -11,25 +11,23 @@ void date_au_char(date deadline)
    printf("| %02d:%02d %02d/%02d/%4d  |", deadline.min, deadline.heure, deadline.jour, deadline.mois, deadline.anne);
 }
 
-
-void status_au_char(int status){
+void status_au_char(int status)
+{
    char tmpchar[30];
- if (status == 1)
-         {
-            strcpy(tmpchar, "a realiser");
-         }
-         else if (status == 2)
-         {
-            strcpy(tmpchar, "en cours de realisation");
-         }
-         else
-         {
-            strcpy(tmpchar, "finalisee");
-         }
-         printf("|%-30s", tmpchar);
+   if (status == 1)
+   {
+      strcpy(tmpchar, "a realiser");
+   }
+   else if (status == 2)
+   {
+      strcpy(tmpchar, "en cours de realisation");
+   }
+   else
+   {
+      strcpy(tmpchar, "finalisee");
+   }
+   printf("|%-30s", tmpchar);
 }
-
-
 
 void Afficher_tous_taches_par_user_id(int id)
 {
@@ -43,21 +41,27 @@ void Afficher_tous_taches_par_user_id(int id)
    printf("\n==================================================================================================\n");
    fermer_fichier(todo_file);
    int op = ouvrir_fichier("./test.txt", "rb");
-   
+
    if (op == 1)
    {
 
       while (fread(&tache_, sizeof(tache), 1, todo_file))
       {
 
-         printf("   %d  |%-30s", tache_.id, tache_.titre);
+         printf("               |%-30s", tache_.titre);
 
          //   à réaliser : 1  en cours de réalisation : 2  finalisée: 3
-          status_au_char(tache_.status);
-        
+         status_au_char(tache_.status);
+
          date_au_char(tache_.deadline);
 
-         printf("\n--------------------------------------------------------------------------------------------------\n");
+         printf("\n               ----------------------------------------------------------------------------------\n");
+         printf("   %d  |  description :                                                                  |  \n", tache_.id);
+         // printf("\n               ===================================================================================\n");
+         printf("               | %-80s|  ", tache_.desc);
+
+         printf("\n==================================================================================================\n");
+         // printf("\n--------------------------------------------------------------------------------------------------\n");
       }
    }
    else
